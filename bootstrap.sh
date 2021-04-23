ff5d6785fab0221bea1e0e03504589e93777db3f
Rafael Souza <razuos@protonmail.com>
2021-04-23 00:13:27 -0300

    feat: better lifecycle

--- bootstrap.sh
+++ bootstrap.sh
@@ -25,6 +25,18 @@ file_env() {
   unset "$fileVar"
 }
 
+file_env 'APP_USER' 'app'
+file_env 'APP_PASSWORD' 'password'
+file_env 'APP_DB' 'app'
+
+file_env 'POSTGRES_USER' 'postgres'
+file_env 'POSTGRES_PASSWORD' 'password'
+
+file_env 'REPLICATION_USER' 'bdrsync'
+file_env 'REPLICATION_PASSWORD' 'default'
+file_env 'REPLICATION_MAIN' ''
+file_env 'REPLICATION_SYNC' ''
+
 #usage: sql HOST USER PASS args
 sql() {
   export PGPASSWORD="$3"
@@ -33,28 +45,42 @@ sql() {
 }
 
 peer1SQL() {
-  sql "$REPLICATION_PEER1" "$POSTGRES_USER" "$POSTGRES_PASSWORD" "$@"
+  sql "$REPLICATION_MAIN" "$POSTGRES_USER" "$POSTGRES_PASSWORD" "$@"
 }
 
 peer2SQL() {
-  sql "$REPLICATION_PEER2" "$POSTGRES_USER" "$POSTGRES_PASSWORD" "$@"
+  sql "$REPLICATION_SYNC" "$POSTGRES_USER" "$POSTGRES_PASSWORD" "$@"
+}
+
+is_replication_working() {
+  echo
+  echo "Testing replication."
+  echo
+
+  if ! peer2SQL --dbname="$APP_DB" <<<"SELECT * FROM sync_test;" | grep -q row; then
+    echo "Test table does not exist, replication is definetly not working."
+    return 1
+  fi
+  
+  num=$RANDOM
+  echo "Random number generated: $num"
+
+  echo "Inserting into main-db."
+  peer1SQL --dbname="$APP_DB" <<<"INSERT INTO sync_test(num) VALUES ($num);"
+  echo "Waiting for replication to be done."
+  peer1SQL --dbname="$APP_DB" <<<"SELECT bdr.wait_slot_confirm_lsn(NULL, NULL)"
+
+  echo "Checking if it was replicated."
+  peer2SQL --dbname="$APP_DB" <<<"SELECT * FROM sync_test;"
+
+  if ! peer2SQL --dbname="$APP_DB" <<<"SELECT * FROM sync_test;" | grep -q $num; then
+    return 1
+  else
+    return 0
+  fi
 }
 
 setup_replication() {
-
-  sleep 10
-
-  file_env 'APP_USER' 'app'
-  file_env 'APP_PASSWORD' 'password'
-  file_env 'APP_DB' 'app'
-
-  file_env 'POSTGRES_USER' 'postgres'
-  file_env 'POSTGRES_PASSWORD' 'password'
-
-  file_env 'REPLICATION_USER' 'bdrsync'
-  file_env 'REPLICATION_PASSWORD' 'default'
-  file_env 'REPLICATION_PEER1' ''
-  file_env 'REPLICATION_PEER2' ''
 
   echo
   echo 'Setting up AVAPolos replication'
@@ -76,48 +102,48 @@ setup_replication() {
 
   echo "Creating BDR group."
   peer1SQL --dbname="$APP_DB" <<<"SELECT bdr.bdr_group_create(
-          local_node_name := '$REPLICATION_PEER1',
-          node_external_dsn := 'host=$REPLICATION_PEER1 user=$REPLICATION_USER dbname=$APP_DB password=$REPLICATION_PASSWORD'
+          local_node_name := '$REPLICATION_MAIN',
+          node_external_dsn := 'host=$REPLICATION_MAIN user=$REPLICATION_USER dbname=$APP_DB password=$REPLICATION_PASSWORD'
   );"
   peer1SQL --dbname="$APP_DB" <<<"SELECT bdr.bdr_node_join_wait_for_ready()"
   peer1SQL --dbname="$APP_DB" <<<"SELECT bdr.bdr_nodes.node_status FROM bdr.bdr_nodes;"
 
   echo "Joining BDR group."
   peer2SQL --dbname="$APP_DB" <<<"SELECT bdr.bdr_group_join(
-    local_node_name := '$REPLICATION_PEER2',
-    node_external_dsn := 'host=$REPLICATION_PEER2 user=$REPLICATION_USER dbname=$APP_DB password=$REPLICATION_PASSWORD',
-    join_using_dsn := 'host=$REPLICATION_PEER1 user=$REPLICATION_USER dbname=$APP_DB password=$REPLICATION_PASSWORD'
+    local_node_name := '$REPLICATION_SYNC',
+    node_external_dsn := 'host=$REPLICATION_SYNC user=$REPLICATION_USER dbname=$APP_DB password=$REPLICATION_PASSWORD',
+    join_using_dsn := 'host=$REPLICATION_MAIN user=$REPLICATION_USER dbname=$APP_DB password=$REPLICATION_PASSWORD'
   );"
   peer2SQL --dbname="$APP_DB" <<<"SELECT bdr.bdr_node_join_wait_for_ready()"
   peer2SQL --dbname="$APP_DB" <<<"SELECT bdr.bdr_nodes.node_status FROM bdr.bdr_nodes;"
 
-  # moodle_user varchar(255) not null
-  echo "Creating avapolos_sync table."
-  peer1SQL --dbname="$APP_DB" <<<"CREATE TABLE avapolos_sync (
+  peer1SQL --dbname="$APP_DB" <<<"CREATE TABLE sync_test (
     id serial not null PRIMARY KEY,
-    createdAt timestamptz not null DEFAULT NOW(),
-    instance char(4) not null,
-    iteration int not null,
-    operation char(1) not null
+    num int not null
   );"
+}
 
-  peer1SQL --dbname="$APP_DB" <<<"SELECT bdr.wait_slot_confirm_lsn(NULL, NULL)"
-  peer2SQL --dbname="$APP_DB" <<<"SELECT bdr.wait_slot_confirm_lsn(NULL, NULL)"
-
-  echo "Checking if it was replicated."
-
-  peer2SQL --dbname="$APP_DB" <<<"SELECT * FROM avapolos_sync;"
-
-  if ! [[ -z "$(peer2SQL --dbname="$APP_DB" <<<"SELECT * FROM avapolos_sync;" | grep -o row)" ]]; then
+main() {
+  if is_replication_working; then
     echo
-    echo 'Replication set up successfully.'
+    echo "Replication is already working, skipping."
     echo
   else
+    echo "Replication is not working, configuring."
+    setup_replication
+
+    if ! is_replication_working; then
+      echo
+      echo "Replication is still not working after configuring"
+      echo
+      exit 1
+    fi
+
     echo
-    echo 'Failed to set up replication.'
+    echo "Replication is working."
     echo
-    exit 1
+    exit 0
   fi
 }
 
-setup_replication
+main
